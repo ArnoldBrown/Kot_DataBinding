198b13b790981e7ea84e1f1262e87204fbde17e0
ArnoldBrown <arnoldbrown033@gmail.com>
2025-01-06 12:15:23 +0530

    Add_DataBinging

--- app/src/main/java/com/mrbee/starterproject/MainActivity.kt
+++ app/src/main/java/com/mrbee/starterproject/MainActivity.kt
@@ -5,21 +5,26 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.mrbee.starterproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val button = findViewById<Button>(R.id.submit_button)
-        button.setOnClickListener {
+//        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+//        val button = findViewById<Button>(R.id.submit_button)
+        binding.submitButton.setOnClickListener {
             displayGreeting()
         }
     }
 
     private fun displayGreeting() {
-        val messageView = findViewById<TextView>(R.id.greeting_text_view)
-        val nameText = findViewById<EditText>(R.id.name_edit_text)
-
-        messageView.text = "Hello! "+ nameText.text
+//        val messageView = findViewById<TextView>(R.id.greeting_text_view)
+//        val nameText = findViewById<EditText>(R.id.name_edit_text)
+        binding.apply {
+            greetingTextView.text = "Hello! "+ nameEditText.text
+        }
     }
 }
